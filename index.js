6952ab2567d164d8017f81407ffaff6cb860567b
ariana gutierrez <carla_ari.97@live.com>
2024-01-22 00:37:45 +0100

    animal-generator

---
+++ index.js
@@ -0,0 +1,65 @@
+/** @format */
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const express = require('express');
+const app = express();
+const port = 4000; //cambia a 3000 dopo aver finito
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+//the user's request to the server:
+app.use(express.json());
+
+let searchTerm = '';
+
+app.post('/setsearchterm', (request, response) => {
+  const { newSearchValue } = request.body;
+  if (newSearchValue) {
+    searchTerm = newSearchValue;
+    response.json({ success: true, searchTerm });
+  } else {
+    response
+      .status(400)
+      .json({ success: false, error: 'The new search term was not provided!' });
+  }
+});
+
+//access to the APIs:
+app.get('/animalname', async (request, response) => {
+  const fetchAPI = await fetch(
+    `https://animals-by-api-ninjas.p.rapidapi.com/v1/animals?name=${searchTerm}`,
+    {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': process.env.API_KEY_NAME,
+        'X-RapidAPI-Host': 'animals-by-api-ninjas.p.rapidapi.com',
+      },
+    }
+  );
+
+  const animalNameResponse = await fetchAPI.json();
+  console.log(animalNameResponse);
+  response.json(animalNameResponse);
+});
+
+app.get('/animalimage', async (request, response) => {
+  const fetchAPI = await fetch(
+    `https://api.pexels.com/v1/search?query=${searchTerm}&per_page=20&total_results=30`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: process.env.API_KEY_IMAGE,
+      },
+    }
+  );
+
+  const animalImageResponse = await fetchAPI.json();
+  console.log(animalImageResponse);
+  response.json(animalImageResponse);
+});
---
+++ public/script.js
@@ -0,0 +1,95 @@
+const form = document.getElementById('searchForm');
+const newSearchTerm = document.getElementById('newSearchTerm');
+const errorSearch = document.querySelector('.errorSearch');
+const errorName = document.querySelector('.errorName');
+
+//to declare the one-word rule allowed in the search
+form.addEventListener('submit', function (event) {
+  const value = newSearchTerm.value.trim();
+  const words = value.split(' ');
+
+  if (words.length > 1) {
+    event.preventDefault();
+    errorName.style.display = 'block';
+    errorSearch.style.display = 'block';
+    console.log('Please enter only one word!')
+  } else {
+    errorName.style.display = 'none';
+    errorSearch.style.display = 'none';
+  }
+});
+
+//to generate the animal's info
+document
+  .querySelector('#searchForm')
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const newSearchValue = newSearchTerm.value;
+
+    const updateResponse = await fetch('/setsearchterm', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ newSearchValue }),
+    });
+
+    const updateData = await updateResponse.json();
+    if (updateData.success) {
+      await getAnimalName();
+    } else {
+      console.error(updateData.error);
+    }
+  });
+
+//to remove the previous data
+document.querySelector('#btnLoad').addEventListener('click', () => {
+  if (document.querySelector('#animalName') !== null) {
+    document.querySelector('#animalName').remove();
+  }
+  if (document.querySelector('#animalImage') !== null) {
+    document.querySelector('#animalImage').remove();
+  }
+});
+
+//to get the animal's name
+async function getAnimalName() {
+  const response = await fetch('/animalname');
+  const data = await response.json();
+  
+  if (!data || data.length === 0) {
+    console.log('Animal not available!')
+    errorName.style.display = 'block';
+    return
+  } else {
+    errorName.style.display = 'none';
+  }
+
+  let animalItems = data[Math.floor(Math.random() * data.length)];
+  let animalName = animalItems.name;
+  console.log(animalName);
+
+  let animalNameDiv = document.createElement('div');
+  animalNameDiv.id = 'animalName';
+  animalNameDiv.textContent = animalName;
+  document.querySelector('#animalWrapper').appendChild(animalNameDiv);
+
+  getAnimalImage();
+}
+
+//to get the animal's image
+async function getAnimalImage() {
+  const response = await fetch('/animalimage');
+  const data = await response.json();
+  let animalImage = data.photos[Math.floor(Math.random() * data.photos.length)];
+  let animalImageURL = animalImage.src.medium;
+  let animalAlt = animalImage.alt;
+  console.log(animalImage);
+
+  let img = document.createElement('img');
+  img.id = 'animalImage';
+  img.src = animalImageURL;
+  img.alt = animalAlt;
+  document.querySelector('#animalWrapper').appendChild(img);
+}
