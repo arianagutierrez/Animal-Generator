a413442df1b84eaa072096825730ec66ea23adf1
ariana gutierrez <carla_ari.97@live.com>
2024-01-23 09:55:03 +0100

    update file

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.listen(port, () => {
 app.use(express.json());
 
 let searchTerm = '';
+
 app.post('/setsearchterm', (request, response) => {
   const { newSearchValue } = request.body;
   if (newSearchValue) {
